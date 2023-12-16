c4c8e76d5e3309ab58a6ea4cca8d391ef3c17418
sashika20643 <sashikadulaj20643@gmail.com>
2023-12-16 22:59:34 +0530

    final update

--- app.py
+++ app.py
@@ -201,63 +201,7 @@ def upload_sub(id):
     print(f"Public URL for subtitle: {public_url}")
     
     return public_url
-    
-# mp_holistic = mp.solutions.holistic # Holistic model
-# mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
-# def mediapipe_detection(image, model):
-#   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-#   image.flags.writeable = False                   # Image is no longer writeable
-#   results = model.process(image)                  # Make prediction
-#   image.flags.writeable = True                    # Image is now writeable
-#   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
-#   return image, results
-# def draw_landmarks(image, results):
-#     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
-#     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-#     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw-left hand connections
-#     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
-
-# def draw_styled_landmarks (image, results):
-#     #Draw face connections
-#     mp_drawing.draw_landmarks(image, results. face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-#                               mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-#                               mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-#                              )
-#     # Draw pose connections
-#     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-#                               mp_drawing.DrawingSpec(color=(88,22,10), thickness=2, circle_radius=4),
-#                               mp_drawing.DrawingSpec (color=(80, 44, 121), thickness=2, circle_radius=2)
-#                              )
-#     #Draw Left hand connections
-#     mp_drawing.draw_landmarks (image, results. left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-#                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-#                                mp_drawing.DrawingSpec (color=(121,44,250), thickness=2, circle_radius=2) 
-#                               )
-#     #Draw right hand connections
-#     mp_drawing.draw_landmarks (image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-#                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-#                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-#                               )
-
-# def extract_keypoints(results):
-#     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-#     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(21*3)
-#     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-#     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-#     return np.concatenate([pose, face, lh, rh])
-#     # Function to find the index of the first available camera
-# def find_available_camera():
-#     for i in range(10):  # Try the first 10 camera indices (adjust as needed)
-#         cap = cv2.VideoCapture(i)
-#         if cap.isOpened():
-#             cap.release()  # Release the camera immediately
-#             return i
-#     return None  # No available cameras found
-
-
-    
-    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -309,7 +253,8 @@ def upload_file():
 @app.route('/mongo/<string:file_id>', methods=['GET'])
 def mongo(file_id):
         file_id_to_update = "652bf32459fbeea9aea09f1f"
-        get_video(file_id)
+        video_url=get_video(file_id)
+
         url=upload_sub(file_id)
         update_subtitle_status(file_id,url)
 
