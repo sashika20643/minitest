3cc3238f7619d5d21ffa6bf61046e26aca14e834
sashika20643 <sashikadulaj20643@gmail.com>
2023-12-17 03:25:05 +0530

    update test v1

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from pymongo import MongoClient
 from bson import ObjectId
 from urllib.parse import quote
 from flask_cors import CORS
-import mediapipe as mp
-import tensorflow as tf
+# import mediapipe as mp
+# import tensorflow as tf
 
 
 
@@ -139,51 +139,51 @@ def update_subtitle_status(file_id,url):
 
 
 
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+# mp_holistic = mp.solutions.holistic # Holistic model
+# mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
-def mediapipe_detection(image, model):
-  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-  image.flags.writeable = False                   # Image is no longer writeable
-  results = model.process(image)                  # Make prediction
-  image.flags.writeable = True                    # Image is now writeable
-  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
-  return image, results
+# def mediapipe_detection(image, model):
+#   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+#   image.flags.writeable = False                   # Image is no longer writeable
+#   results = model.process(image)                  # Make prediction
+#   image.flags.writeable = True                    # Image is now writeable
+#   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
+#   return image, results
 
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw-left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
+# def draw_landmarks(image, results):
+#     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
+#     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+#     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw-left hand connections
+#     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
 
-def draw_styled_landmarks (image, results):
-    #Draw face connections
-    mp_drawing.draw_landmarks(image, results. face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             )
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(88,22,10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec (color=(80, 44, 121), thickness=2, circle_radius=2)
-                             )
-    #Draw Left hand connections
-    mp_drawing.draw_landmarks (image, results. left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                               mp_drawing.DrawingSpec (color=(121,44,250), thickness=2, circle_radius=2) 
-                              )
-    #Draw right hand connections
-    mp_drawing.draw_landmarks (image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                               mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                              )
+# def draw_styled_landmarks (image, results):
+#     #Draw face connections
+#     mp_drawing.draw_landmarks(image, results. face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+#                               mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+#                               mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+#                              )
+#     # Draw pose connections
+#     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+#                               mp_drawing.DrawingSpec(color=(88,22,10), thickness=2, circle_radius=4),
+#                               mp_drawing.DrawingSpec (color=(80, 44, 121), thickness=2, circle_radius=2)
+#                              )
+#     #Draw Left hand connections
+#     mp_drawing.draw_landmarks (image, results. left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+#                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+#                                mp_drawing.DrawingSpec (color=(121,44,250), thickness=2, circle_radius=2) 
+#                               )
+#     #Draw right hand connections
+#     mp_drawing.draw_landmarks (image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+#                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+#                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+#                               )
     
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(21*3)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
+# def extract_keypoints(results):
+#     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+#     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(21*3)
+#     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+#     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+#     return np.concatenate([pose, face, lh, rh])
     
 
 
@@ -322,73 +322,84 @@ def model():
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Specify the path to the model file
+
     model_path = os.path.join(base_dir, 'action.h5')
-    model = tf.keras.models.load_model(model_path)
-    actions = np.array(['hello', 'thanks', 'iloveyou'])
-    # 1. New detection variables
-    sentence = []
-    sequence = []
-    predictions = []
-    threshold = 0.7
-    val = ''
+    
+    
 
-    cap = cv2.VideoCapture(os.path.join(base_dir, 'v.mp4'))
+    # Check if the file exists
+    if os.path.exists(model_path):
+        print(f"The model file exists at: {os.path.abspath(model_path)}")
+    # model = tf.keras.models.load_model(model_path)
+    # print(f"Model path: {model_path}")
+    # model = tf.keras.models.load_model(model_path)
 
-    # Set mediopipe model
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
 
-            # Read feed
-            ret, frame = cap.read()
+    # actions = np.array(['hello', 'thanks', 'iloveyou'])
+    # # 1. New detection variables
+    # sentence = []
+    # sequence = []
+    # predictions = []
+    # threshold = 0.7
+    # val = ''
+
+    # cap = cv2.VideoCapture(os.path.join(base_dir, 'v.mp4'))
+
+    # # Set mediopipe model
+    # with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    #     while cap.isOpened():
+
+    #         # Read feed
+    #         ret, frame = cap.read()
             
-            # Make detections
-            if not ret:
-                break  # End of video, break out of the loop
+    #         # Make detections
+    #         if not ret:
+    #             break  # End of video, break out of the loop
 
-            #Make detections
-            image, results = mediapipe_detection(frame, holistic)
-    #         print(results)
+    #         #Make detections
+    #         image, results = mediapipe_detection(frame, holistic)
+    # #         print(results)
 
-            #Draw Landmarks
+    #         #Draw Landmarks
 
-            #2. Prediction Logic
-            keypoints = extract_keypoints(results)
-    #         sequence.insert(0,keypoints)
-            sequence.append(keypoints)
-            sequence = sequence[-29:]
-    #         print(len(sequence))
+    #         #2. Prediction Logic
+    #         keypoints = extract_keypoints(results)
+    # #         sequence.insert(0,keypoints)
+    #         sequence.append(keypoints)
+    #         sequence = sequence[-29:]
+    # #         print(len(sequence))
 
-            if len(sequence) == 29:
-                res = model.predict(np.expand_dims(sequence, axis=0))[0]
-                if (val != actions [np.argmax(res)]):
-                    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-                    print(actions [np.argmax(res)])
-                val = actions [np.argmax(res)]
-                predictions.append(np.argmax(res))
+    #         if len(sequence) == 29:
+    #             res = model.predict(np.expand_dims(sequence, axis=0))[0]
+    #             if (val != actions [np.argmax(res)]):
+    #                 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    #                 print(actions [np.argmax(res)])
+    #             val = actions [np.argmax(res)]
+    #             predictions.append(np.argmax(res))
 
-            #3. Viz Logic
-                if np.unique(predictions[-10:])[0]== np.argmax(res):
-                    if res[np.argmax(res)] > threshold:
-                        if len(sentence) > 0:
-                            if actions[np.argmax(res)] != sentence[-1]:
-                                sentence.append(actions[np.argmax(res)])
-                        else:
-                            sentence.append(actions[np.argmax(res)])
-                if len(sentence) > 5:
-                    sentence = sentence[-5:]
+    #         #3. Viz Logic
+    #             if np.unique(predictions[-10:])[0]== np.argmax(res):
+    #                 if res[np.argmax(res)] > threshold:
+    #                     if len(sentence) > 0:
+    #                         if actions[np.argmax(res)] != sentence[-1]:
+    #                             sentence.append(actions[np.argmax(res)])
+    #                     else:
+    #                         sentence.append(actions[np.argmax(res)])
+    #             if len(sentence) > 5:
+    #                 sentence = sentence[-5:]
             
-            cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
-            cv2.putText(image,''.join(sentence), (3,30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    #         cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+    #         cv2.putText(image,''.join(sentence), (3,30),
+    #                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-            # Show to screen
-            cv2.imshow('OpenCV Feed', image)
+    #         # Show to screen
+    #         cv2.imshow('OpenCV Feed', image)
 
-            # Break gracefully
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-        cap.release()
-        cv2.destroyAllWindows()
+    #         # Break gracefully
+    #         if cv2.waitKey(10) & 0xFF == ord('q'):
+    #             break
+    #     cap.release()
+    #     cv2.destroyAllWindows()
     return "done"
 
 # @app.route('/posts/<string:post_id>', methods=['GET'])
