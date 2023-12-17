e5f28389b5b0c58d44405368fb724be1930ca3a3
sashika20643 <sashikadulaj20643@gmail.com>
2023-12-17 13:08:42 +0530

    test v4

--- app.py
+++ app.py
@@ -157,33 +157,9 @@ def mediapipe_detection(image, model):
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
   return image, results
 
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw-left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
 
-def draw_styled_landmarks (image, results):
-    #Draw face connections
-    mp_drawing.draw_landmarks(image, results. face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             )
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(88,22,10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec (color=(80, 44, 121), thickness=2, circle_radius=2)
-                             )
-    #Draw Left hand connections
-    mp_drawing.draw_landmarks (image, results. left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                               mp_drawing.DrawingSpec (color=(121,44,250), thickness=2, circle_radius=2) 
-                              )
-    #Draw right hand connections
-    mp_drawing.draw_landmarks (image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                               mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                              )
+
+
     
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
@@ -384,8 +360,7 @@ def model():
             # Make detections
             image, results = mediapipe_detection(frame, holistic)
 
-            # Draw Landmarks
-            draw_styled_landmarks(image, results)
+           
 
             # Prediction Logic
             keypoints = extract_keypoints(results)
@@ -430,7 +405,6 @@ def model():
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
             # Show to screen
-            cv2.imshow('OpenCV Feed', image)
 
             # Break gracefully
             if cv2.waitKey(10) & 0xFF == ord('q'):
